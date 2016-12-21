59c9e6af9db142afb8c70b1fb9d56bb97b91bb0d
Ashish Kumar Sai <ashishkumarsai@me.com>
2016-12-21 10:07:46 +0530

    Created the login View controller and implemented Sign Up functionality

--- On The Map/ViewController.swift
+++ On The Map/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  On The Map
 //
 //  Created by Ashutosh Kumar Sai on 20/12/16.
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var statusText: UITextField!
+    
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var passWord: UITextField!
+    
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        if let signupURL = URL(string: "https://auth.udacity.com/sign-up?next=https://in.udacity.com/")
+        {
+            UIApplication.shared.openURL(signupURL)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
