66f2a3d8a6b84cd610cd763182c7d5661aa97bdb
Ashish Kumar Sai <ashishkumarsai@me.com>
2016-12-21 22:10:36 +0530

    Table View Controller working fine now Only gotta work on two more things 1) Add new data and 2) Log Out

--- On The Map/tableViewController.swift
+++ On The Map/tableViewController.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+
+class tableViewController : UITableViewController
+{
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellReuseIdentifier = "reuseMe"
+        let newpoint = parseClient.sharedInstance().point[(indexPath as NSIndexPath).row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)
+        
+        cell!.textLabel!.text = newpoint.fullName
+        return cell!
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return parseClient.sharedInstance().point.count
+    }
+    
+    
+    //Opens the mediaURL in Safari when a table cell is tapped.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UIApplication.shared.openURL(URL(string: parseClient.sharedInstance().point[(indexPath as NSIndexPath).row].mediaURL)!)
+    }
+}
